5b723da01b6509ae5f1274a7a155ac239fd976c9
anshu <anshu.iiita.10@gmail.com>
2018-12-01 10:08:27 -0600

    Project creation

---
+++ cmake-build-debug/CMakeFiles/malloc_library.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/malloc_library.dir/main.c.o"
+  "malloc_library.pdb"
+  "malloc_library"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/malloc_library.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
